b37a35860aeb46de09afbcdc89702b304728358c
Rafael <rafael.silva@cspautomacao.com.br>
2023-04-06 09:22:49 -0300

    atualização

--- pages/[tenant]/order/[orderid].tsx
+++ pages/[tenant]/order/[orderid].tsx
@@ -33,25 +33,33 @@ const OrderID = (data: Props) => {
 
     const formatter = useFormatter();
     const router = useRouter();
+
+    useEffect(() => {
+        if(data.order.status !== 'delivered'){
+            setTimeout(() => {
+                router.reload();
+            },60000); //60 seconds
+        }
+    }, []);
     
     const orderStatusList = {
         preparing: {
             label: 'Preparando',
-            longLabel: 'Preparando o seu pedido...',
+            longLabel: 'Preparando o seu pedido!',
             backgroundColor: '#FEFAE6',
             fontColor: '#D48C34',
             pct: 25
         },
         sent: {
             label: 'Enviado',
-            longLabel: 'Enviamos o seu pedido...',
+            longLabel: 'Enviamos o seu pedido!',
             backgroundColor: '#F1F3F8',
             fontColor: '#758CBD',
             pct: 75
         },
         delivered: {
             label: 'Entregue',
-            longLabel: 'Seu pedido foi entregue',
+            longLabel: 'Seu pedido foi entregue!',
             backgroundColor: '#F1F8F6',
             fontColor: '#6AB70A',
             pct: 100
@@ -72,6 +80,28 @@ const OrderID = (data: Props) => {
                 color={data.tenant.mainColor}
                 title={`Pedido #${data.order.id}`}
             />
+
+            {data.order.status !== 'delivered' &&
+                <div 
+                    className={styles.statusArea}
+                    style={{backgroundColor: orderStatusList[data.order.status].backgroundColor}}
+                >
+                    <div 
+                        className={styles.statusLongLabel}
+                        style={{color: orderStatusList[data.order.status].fontColor}}
+                    >{orderStatusList[data.order.status].longLabel}</div>
+                    <div className={styles.statusPct}>
+                        <div 
+                            className={styles.statusPctBar}
+                            style={{
+                                width: `${orderStatusList[data.order.status].pct}%`,
+                                backgroundColor: orderStatusList[data.order.status].fontColor
+                            }}
+                        ></div>
+                    </div>
+                    <div className={styles.statusMsg}>Aguardando mudança de status...</div>
+                </div>
+            }
 
             <div className={styles.orderInfoArea}>
                 <div className={styles.orderInfoStatus} style={{
